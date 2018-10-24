e08d1390e0b160161b0c86932a758f1b8c612c14
QianPengJeff <jimpeng1990@hotmail.com>
2018-10-24 22:38:43 +1100

    changed firebase details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC7fS6ZA21mc5pIKkVTlpxkzy1gpDY7Brc",
+    authDomain: "sublime-online-ordering-b5f38.firebaseapp.com",
+    databaseURL: "https://sublime-online-ordering-b5f38.firebaseio.com",
+    projectId: "sublime-online-ordering-b5f38",
+    storageBucket: "sublime-online-ordering-b5f38.appspot.com",
+    messagingSenderId: "568650425361"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
-    apiKey: "AIzaSyBz9lAStcYsk_-na5u-E1pyruO5hStvPbY",
-    authDomain: "sublime-online-ordering.firebaseapp.com",
-    databaseURL: "https://sublime-online-ordering.firebaseio.com",
-    projectId: "sublime-online-ordering",
-    storageBucket: "sublime-online-ordering.appspot.com",
-    messagingSenderId: "503954406384"
+    apiKey: "AIzaSyC7fS6ZA21mc5pIKkVTlpxkzy1gpDY7Brc",
+    authDomain: "sublime-online-ordering-b5f38.firebaseapp.com",
+    databaseURL: "https://sublime-online-ordering-b5f38.firebaseio.com",
+    projectId: "sublime-online-ordering-b5f38",
+    storageBucket: "sublime-online-ordering-b5f38.appspot.com",
+    messagingSenderId: "568650425361"
   }
 };
