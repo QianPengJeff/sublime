a2b80bcc42e0659da6354fac81307c0bd74c6ba5
Sakshi Tewari <sakshi.tewari@carsales.com.au>
2018-10-25 16:22:22 +1100

    home page onn click go to cafe

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -8,7 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class HomePageComponent implements OnInit {
 
   Images = ['assets/images/sublime_1.jpg', 'assets/images/sublime_2.jpg', 'assets/images/sublime_3.jpg'];
-  constructor() { }
+  constructor(private router: Router) { }
+
+  NavToCafe = () => {
+    this.router.navigateByUrl('/cafe');
+  }
 
   ngOnInit() {
   }
